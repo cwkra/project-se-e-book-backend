f3160545f9209b93d2bff61303981e605eea0ea0
Parn panisra <Panisra.w@ku.th>
2022-11-08 19:45:54 +0700

    cucumber

---
+++ src/test/java/th/ac/ku/book/testingWithSelenium/AddBookTest.java
@@ -0,0 +1,99 @@
+package th.ac.ku.book.testingWithSelenium;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class AddBookTest {
+    @LocalServerPort
+    private Integer port;
+
+    private static WebDriver driver;
+    private static WebDriverWait wait;
+
+    @BeforeAll
+    public static void beforeAll() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 1000);
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        driver.get("http://localhost:" + "8091" + "/book/add");
+    }
+
+    @AfterEach
+    public void afterEach() throws InterruptedException {
+        Thread.sleep(3000);
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        driver.quit();
+    }
+
+
+    //test add ผ่าน แต่ price ไม่ตรง
+    @Test
+    void testAddBook() {
+
+        WebElement usernameField = wait.until(webDriver ->
+                webDriver.findElement(By.id("inputUsername")));
+        WebElement passwordField = driver.findElement(By.id("inputPassword"));
+
+        WebElement submitButton1 = driver.findElement(By.id("submit-button"));
+
+
+        //ก่อนรัน test เปลี่ยนชื่อและรหัสผ่านตามใน database table user
+        usernameField.sendKeys("parn");
+        passwordField.sendKeys("1104200178882");
+
+        submitButton1.click();
+
+
+        WebElement submitButton2 = driver.findElement(By.id("addBook-button"));
+        submitButton2.click();
+
+
+
+        WebElement nameField = wait.until(webDriver ->
+                webDriver.findElement(By.id("inputName")));
+        WebElement priceField = driver.findElement(By.id("inputPrice"));
+        WebElement categoryField = driver.findElement(By.id("inputCategory"));
+        WebElement authorField = driver.findElement(By.id("inputAuthor"));
+
+        WebElement submitButton = driver.findElement(By.id("submit-button"));
+
+        //clearText
+        priceField.sendKeys("");
+
+        nameField.sendKeys("Clean Code2");
+        priceField.sendKeys("2145");
+        categoryField.sendKeys("Science Fiction");
+        authorField.sendKeys("Robert Martin");
+
+        submitButton.click();
+
+        WebElement firstTd = wait.until(webDriver ->
+                webDriver.findElement(By.tagName("td")));
+        assertEquals("Clean Code2", firstTd.getText());
+
+    }
+
+
+
+
+}
+
+
--- src/test/java/th/ac/ku/book/testingWithSelenium/LoginTest.java
+++ src/test/java/th/ac/ku/book/testingWithSelenium/LoginTest.java
@@ -1,4 +1,4 @@
-package th.ac.ku.book;
+package th.ac.ku.book.testingWithSelenium;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
@@ -7,14 +7,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class AddBookTest {
+public class LoginTest {
     @LocalServerPort
     private Integer port;
 
@@ -30,7 +27,7 @@ public class AddBookTest {
 
     @BeforeEach
     public void beforeEach() {
-        driver.get("http://localhost:" + "8091" + "/book/add");
+        driver.get("http://localhost:" + "8091" + "/login" );
     }
 
     @AfterEach
@@ -43,57 +40,27 @@ public class AddBookTest {
         driver.quit();
     }
 
-
-    //test add ผ่าน แต่ price ไม่ตรง
     @Test
-    void testAddBook() {
+    void testLogin() {
 
         WebElement usernameField = wait.until(webDriver ->
                 webDriver.findElement(By.id("inputUsername")));
         WebElement passwordField = driver.findElement(By.id("inputPassword"));
 
-        WebElement submitButton1 = driver.findElement(By.id("submit-button"));
+        WebElement submitButton = driver.findElement(By.id("submit-button"));
 
 
         //ก่อนรัน test เปลี่ยนชื่อและรหัสผ่านตามใน database table user
         usernameField.sendKeys("parn");
         passwordField.sendKeys("1104200178882");
 
-        submitButton1.click();
-
-
-        WebElement submitButton2 = driver.findElement(By.id("addBook-button"));
-        submitButton2.click();
-
-
-
-        WebElement nameField = wait.until(webDriver ->
-                webDriver.findElement(By.id("inputName")));
-        WebElement priceField = driver.findElement(By.id("inputPrice"));
-        WebElement categoryField = driver.findElement(By.id("inputCategory"));
-        WebElement authorField = driver.findElement(By.id("inputAuthor"));
-
-        WebElement submitButton = driver.findElement(By.id("submit-button"));
-
-        //clearText
-        priceField.sendKeys("");
-
-        nameField.sendKeys("Clean Code2");
-        priceField.sendKeys("2145");
-        categoryField.sendKeys("Science Fiction");
-        authorField.sendKeys("Robert Martin");
-
         submitButton.click();
 
-        WebElement firstTd = wait.until(webDriver ->
-                webDriver.findElement(By.tagName("td")));
-        assertEquals("Clean Code2", firstTd.getText());
+
+
+
 
     }
 
 
-
-
 }
-
-
